0cd4cd333cb9e745591c103076fba1be616ded1c
bomzae <tmzl235@naver.com>
2026-05-01 03:25:30 +0900

    feat: PaymentAmount VO 로직 작성

---
+++ src/main/java/com/yeoljeong/tripmate/payment/domain/exception/PaymentErrorCode.java
@@ -0,0 +1,27 @@
+package com.yeoljeong.tripmate.payment.domain.exception;
+
+import com.yeoljeong.tripmate.exception.constants.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum PaymentErrorCode implements ErrorCode {
+    INVALID_REQUESTED_AMOUNT(HttpStatus.BAD_REQUEST, "결제 요청 금액은 0보다 커야 합니다."),
+    INVALID_APPROVED_AMOUNT(HttpStatus.BAD_REQUEST, "결제 요청 금액과 승인 요청 금액이 일치하지 않습니다."),
+    INVALID_CANCELED_AMOUNT(HttpStatus.BAD_REQUEST, "취소 금액은 0보다 커야 합니다.");
+
+    private final HttpStatus status;
+    private final String description;
+
+    @Override
+    public int getCode() {
+        return this.status.value();
+    }
+
+    @Override
+    public String getMessage() {
+        return this.description;
+    }
+}
