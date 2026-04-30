a42db685384de1b8de4e94ae68d9336c72aceb85
bomzae <tmzl235@naver.com>
2026-05-01 03:26:11 +0900

    feat: TossPayment VO 로직 작성

--- src/main/java/com/yeoljeong/tripmate/payment/domain/exception/PaymentErrorCode.java
+++ src/main/java/com/yeoljeong/tripmate/payment/domain/exception/PaymentErrorCode.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum PaymentErrorCode implements ErrorCode {
     INVALID_REQUESTED_AMOUNT(HttpStatus.BAD_REQUEST, "결제 요청 금액은 0보다 커야 합니다."),
     INVALID_APPROVED_AMOUNT(HttpStatus.BAD_REQUEST, "결제 요청 금액과 승인 요청 금액이 일치하지 않습니다."),
-    INVALID_CANCELED_AMOUNT(HttpStatus.BAD_REQUEST, "취소 금액은 0보다 커야 합니다.");
+    INVALID_CANCELED_AMOUNT(HttpStatus.BAD_REQUEST, "취소 금액은 0보다 커야 합니다."),
+    INVALID_TOSS_ORDER_ID(HttpStatus.BAD_REQUEST, "토스 주문번호가 유효하지 않습니다."),
+    INVALID_PAYMENT_KEY(HttpStatus.BAD_REQUEST, "토스 결제 키가 유효하지 않습니다.");
 
     private final HttpStatus status;
     private final String description;
