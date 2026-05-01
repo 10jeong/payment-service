b75fc3f5328f0a03f55694b677c4f3107c19237e
bomzae <tmzl235@naver.com>
2026-05-02 03:14:46 +0900

    chore: toss 호출 시 base url 설정 오류 수정

--- src/main/java/com/yeoljeong/tripmate/payment/infrastructure/external/TossPaymentFeignClient.java
+++ src/main/java/com/yeoljeong/tripmate/payment/infrastructure/external/TossPaymentFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "tossPaymentFeignClient", url = "${payment.toss.base-url}", configuration = TossPaymentFeignConfig.class)
+@FeignClient(name = "tossPaymentFeignClient", url = "${toss.payments.base-url}", configuration = TossPaymentFeignConfig.class)
 public interface TossPaymentFeignClient {
 
     @PostMapping("/v1/payments/confirm")
