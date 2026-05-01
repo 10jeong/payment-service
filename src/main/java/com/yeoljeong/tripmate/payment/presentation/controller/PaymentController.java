d6b2a96e72823e301ddd0d0369c39d901f497616
bomzae <tmzl235@naver.com>
2026-05-02 02:18:17 +0900

    feat: 결제 승인 Controller 로직 구현

--- src/main/java/com/yeoljeong/tripmate/payment/presentation/controller/PaymentController.java
+++ src/main/java/com/yeoljeong/tripmate/payment/presentation/controller/PaymentController.java
@@ -1,8 +1,12 @@
 package com.yeoljeong.tripmate.payment.presentation.controller;
 
+import com.yeoljeong.tripmate.payment.application.dto.command.ConfirmPaymentCommand;
+import com.yeoljeong.tripmate.payment.application.dto.result.ConfirmPaymentResult;
 import com.yeoljeong.tripmate.payment.application.dto.result.CreatePaymentResult;
 import com.yeoljeong.tripmate.payment.application.service.command.PaymentCommandService;
+import com.yeoljeong.tripmate.payment.presentation.dto.request.ConfirmPaymentRequest;
 import com.yeoljeong.tripmate.payment.presentation.dto.request.CreatePaymentRequest;
+import com.yeoljeong.tripmate.payment.presentation.dto.response.ConfirmPaymentResponse;
 import com.yeoljeong.tripmate.payment.presentation.dto.response.CreatePaymentResponse;
 import com.yeoljeong.tripmate.response.ApiResponse;
 import com.yeoljeong.tripmate.response.constants.CommonSuccessCode;
@@ -23,5 +27,13 @@ public class PaymentController {
         CreatePaymentResult result = commandService.createPayment(userId, request.orderId());
 
         return ApiResponse.success(CommonSuccessCode.OK, CreatePaymentResponse.from(result));
+    }
+
+    @PostMapping("/confirm")
+    public ApiResponse<ConfirmPaymentResponse> confirmPayment(@RequestHeader("X-User-Id") UUID userId, @RequestBody ConfirmPaymentRequest request) {
+        ConfirmPaymentCommand command = new ConfirmPaymentCommand(request.paymentKey(), request.tossOrderId(), request.amount());
+        ConfirmPaymentResult result = commandService.confirmPayment(userId, command);
+
+        return ApiResponse.success(CommonSuccessCode.OK, ConfirmPaymentResponse.from(result));
     }
 }
