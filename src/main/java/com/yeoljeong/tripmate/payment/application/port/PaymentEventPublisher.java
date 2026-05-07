e1496b1acac6d95c1ce6173b28bb944075de050b
bomzae <tmzl235@naver.com>
2026-05-07 15:29:54 +0900

    chore: 기존 이벤트 발행 로직 중, 사용되지 않는 파일 제거

--- src/main/java/com/yeoljeong/tripmate/payment/application/port/PaymentEventPublisher.java
+++
@@ -1,9 +0,0 @@
-package com.yeoljeong.tripmate.payment.application.port;
-
-import com.yeoljeong.tripmate.event.PaymentCompletedEvent;
-import com.yeoljeong.tripmate.event.PaymentFailedEvent;
-
-public interface PaymentEventPublisher {
-    void publish(PaymentCompletedEvent event);
-    void publish(PaymentFailedEvent event);
-}
