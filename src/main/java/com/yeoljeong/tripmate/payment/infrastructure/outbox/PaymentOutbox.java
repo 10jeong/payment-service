35c9f2c4dfc35b6106a473e16eae0c1fd1a1ec47
bomzae <tmzl235@naver.com>
2026-05-07 15:00:06 +0900

    feat: payment 이벤트용 outbox 테이블 작성

---
+++ src/main/java/com/yeoljeong/tripmate/payment/infrastructure/outbox/PaymentOutbox.java
@@ -0,0 +1,21 @@
+package com.yeoljeong.tripmate.payment.infrastructure.outbox;
+
+import com.yeoljeong.tripmate.domain.Outbox;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "payment_outbox")
+public class PaymentOutbox extends Outbox {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    public static PaymentOutbox create(String topic, String payload) {
+        PaymentOutbox outbox = new PaymentOutbox();
+        Outbox.init(outbox, topic, payload);
+        return outbox;
+    }
+}
