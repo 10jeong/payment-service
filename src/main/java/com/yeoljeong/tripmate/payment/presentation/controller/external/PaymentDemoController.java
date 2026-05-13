0b4a807600ed869ee0eb6fa39ebcc0e7f4705165
bomzae <tmzl235@naver.com>
2026-05-13 11:48:14 +0900

    chore: 테스트 페이지 리다이렉트 controller 분리

--- src/main/java/com/yeoljeong/tripmate/payment/presentation/controller/external/PaymentDemoController.java
+++ src/main/java/com/yeoljeong/tripmate/payment/presentation/controller/external/PaymentDemoController.java
@@ -1,6 +1,5 @@
 package com.yeoljeong.tripmate.payment.presentation.controller.external;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,14 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/payments")
 public class PaymentDemoController {
-
-    @GetMapping("/test-page")
-    public String redirectToPaymentTestPage() {
-        return "redirect:/toss-test-payment.html";
-    }
 
     @GetMapping("/success")
     public String paymentSuccess(
